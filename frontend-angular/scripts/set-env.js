9cf46f37cafdeda70e77f347f04004825b206978
LuFelix <luciano@tiweb.app.br>
2026-09-07 18:41:06 -0300

    fix(env): add graphqlUrl to set-env.js script for production build

--- frontend-angular/scripts/set-env.js
+++ frontend-angular/scripts/set-env.js
@@ -7,18 +7,21 @@ const targetPathDevelopment = './src/environments/environment.development.ts';
 // Valores padrão caso não existam no .env
 const googleClientId = process.env.GOOGLE_CLIENT_ID || 'COLE_SUA_CHAVE_AQUI';
 const apiUrl = process.env.API_URL || 'http://localhost:8080/wp-json';
+const graphqlUrl = process.env.GRAPHQL_URL || (apiUrl.endsWith('/wp-json') ? apiUrl.replace('/wp-json', '/graphql') : 'http://localhost:8080/graphql');
 
 const envConfigFileProduction = `export const environment = {
   production: true,
   googleClientId: '${googleClientId}',
-  apiUrl: '${apiUrl}'
+  apiUrl: '${apiUrl}',
+  graphqlUrl: '${graphqlUrl}'
 };
 `;
 
 const envConfigFileDevelopment = `export const environment = {
   production: false,
   googleClientId: '${googleClientId}',
-  apiUrl: '${apiUrl}'
+  apiUrl: '${apiUrl}',
+  graphqlUrl: '${graphqlUrl}'
 };
 `;
 
