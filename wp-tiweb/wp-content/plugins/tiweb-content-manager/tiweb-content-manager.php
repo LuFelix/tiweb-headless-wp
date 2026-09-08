00feca8b9a8708e73796eba2bc46392b0d9fb832
LuFelix <luciano@tiweb.app.br>
2026-09-08 15:35:23 -0300

    feat(plugin): register ACF field group for material_sped in code (#38)
    - Register 'Dados do Material' local field group via acf_add_local_field_group on acf/init
    - Add 'arquivo_para_download' file upload field with WPGraphQL exposure ('arquivoParaDownload')
    - Remove dependency on database-stored ACF post entries across environments
    
    Testing:
    - Validated PHP syntax with php -l
    - Verified ACF local group registration with acf_get_field_groups() returning 'local: php'
    - Verified WPGraphQL query on materiaisSped returns dadosDoMaterial.arquivoParaDownload

--- wp-tiweb/wp-content/plugins/tiweb-content-manager/tiweb-content-manager.php
+++ wp-tiweb/wp-content/plugins/tiweb-content-manager/tiweb-content-manager.php
@@ -45,3 +45,66 @@ function tiweb_register_materiais_cpt() {
 
     register_post_type('material_sped', $args);
 }
+
+/**
+ * 2. Registra os campos personalizados (ACF) para Materiais SPED
+ */
+add_action('acf/init', 'tiweb_register_materiais_acf_fields');
+function tiweb_register_materiais_acf_fields() {
+    if (!function_exists('acf_add_local_field_group')) {
+        return;
+    }
+
+    acf_add_local_field_group([
+        'key' => 'group_69e98a37cf57f',
+        'title' => 'Dados do Material',
+        'fields' => [
+            [
+                'key' => 'field_69e98a6a91b1b',
+                'label' => 'Arquivo para Download',
+                'name' => 'arquivo_para_download',
+                'aria-label' => '',
+                'type' => 'file',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => [
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ],
+                'return_format' => 'url',
+                'library' => 'all',
+                'min_size' => '',
+                'max_size' => '',
+                'mime_types' => '',
+                'allow_in_bindings' => 0,
+                'show_in_graphql' => 1,
+                'graphql_description' => '',
+                'graphql_field_name' => 'arquivoParaDownload',
+            ],
+        ],
+        'location' => [
+            [
+                [
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'material_sped',
+                ],
+            ],
+        ],
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+        'show_in_rest' => 0,
+        'show_in_graphql' => 1,
+        'graphql_field_name' => 'dadosDoMaterial',
+        'map_graphql_types_from_location_rules' => 0,
+        'graphql_types' => '',
+    ]);
+}
